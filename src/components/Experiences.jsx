7db0f88be7c48590f03b0cf42c0631e381a26077
Lovepratap8936 <130904076+Lovepratap8936@users.noreply.github.com>
2024-09-01 21:52:51 +0530

    Initial commit

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import ques from "../assets/ques.png";
+import Github from "../components/Github";
+export default function Experiences() {
+  return (
+    <div className='experiences'>
+    
+      <h1  className='heading'>Experience</h1>
+      <hr  />
+      <div data-aos='fade-right' className='journey'>
+      <div className='educard'>
+        <h1 className=''>Ajeesh Solutions</h1>
+        <p>Company aims to provide a platform to host and organize hackathons and events.</p>
+        <hr />
+        <p>Web Developer Intern</p>
+        <p>Oct/2021 - Present</p>
+    </div> 
+    </div>
+    <h1 className='heading'>Github Profile</h1>
+      <hr />
+      </div>
+  )
+}
